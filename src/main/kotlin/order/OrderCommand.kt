1b657e20fc204f763d6b6959df5d4006eafd519d
[Raúl Sánchez] <[sanchez.castillo.raul@gmail.com]>
2024-06-24 15:22:36 +0200

    Making drinks: Drink maker makes 1 chocolate with no sugar and therefore no stick (blue)

--- src/main/kotlin/order/OrderCommand.kt
+++ src/main/kotlin/order/OrderCommand.kt
@@ -1,10 +1,9 @@
 package order
 
-class OrderCommand( val command: String) {
+class OrderCommand(private val command: String) {
     fun send() : String {
         return when(command) {
-            "T:1:0" -> "M:T:1:0"
-            "H::" -> "M:H::"
+            "T:1:0", "H::"-> "M:$command"
             else -> throw UnsupportedOperationException("Unsupported command: $command")
         }
     }
