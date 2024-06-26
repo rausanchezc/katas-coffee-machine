3b2beaad8d14e175db234a57013f3e34f704d66a
[Raúl Sánchez] <[sanchez.castillo.raul@gmail.com]>
2024-06-26 07:31:14 +0200

    Making drinks: Drink maker makes 1 tea with 2 sugars and a stick (green)

--- src/main/kotlin/order/OrderCommand.kt
+++ src/main/kotlin/order/OrderCommand.kt
@@ -3,7 +3,7 @@ package order
 class OrderCommand(private val product: String, private val sugar: Int) {
     fun send(): String = when(product.lowercase()) {
             "coffee" -> "C:2:0"
-            "tea" -> "T:1:0"
+            "tea" -> "T:$sugar:0"
             "chocolate" -> "H::"
             else -> throw UnsupportedOperationException()
         }.also { return "M:$it" }
